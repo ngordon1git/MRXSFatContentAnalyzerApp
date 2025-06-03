31b26cbd2b28f0f47c54a320ee0f83225a3de923
ngordon1 <ngordon1@gmail.com>
2025-06-03 11:37:32 +0300

    First commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Clone the repository
+git clone https://github.com/ngordon1git/MRXSFatContentAnalyzerApp.git
+cd MRXSFatContentAnalyzerApp
+
+# Create a virtual environment
+python3 -m venv MRXSFatAnalyzer
+
+# Activate the virtual environment
+source MRXSFatAnalyzer/bin/activate
+
+# Install prerequisites
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Notify user of completion
+echo "Setup is complete. The virtual environment is activated. Run your scripts as needed."
